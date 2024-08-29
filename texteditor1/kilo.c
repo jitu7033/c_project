731cb2b243a539038754e21de101e9004f7b0e2d
jitu7033 <jraj4797@gmail.com>
2024-08-29 22:29:38 +0530

    vertically cursor scroll testing done

--- texteditor1/kilo.c
+++ texteditor1/kilo.c
@@ -268,7 +268,7 @@ void editorRefreshScreen() {
   abAppend(&ab, "\x1b[H", 3);
   editorDrawRows(&ab);
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);  // 
   abAppend(&ab, buf, strlen(buf));
   abAppend(&ab, "\x1b[?25h", 6);
   write(STDOUT_FILENO, ab.b, ab.len);
